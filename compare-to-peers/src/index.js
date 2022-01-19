813d8e092e9f56bc43a2606227cc0f69dc4b9816
Ben2423 <benlappen@gmail.com>
2022-01-18 18:20:11 -0800

    added useContext to Indexj.js

--- compare-to-peers/src/index.js
+++ compare-to-peers/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Context from './pages/utils/providers/context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Context.Provider>
+        <App />
+    </Context.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
