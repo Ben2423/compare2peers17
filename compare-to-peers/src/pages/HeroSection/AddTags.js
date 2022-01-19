ed7900690179fae3757f5d375108c1d78708d6f2
Ben2423 <benlappen@gmail.com>
2022-01-19 09:48:43 -0800

    css for input tag area

--- compare-to-peers/src/pages/HeroSection/AddTags.js
+++ compare-to-peers/src/pages/HeroSection/AddTags.js
@@ -1,11 +1,28 @@
+import styled from 'styled-components';
+import React, {useState} from 'react'
+
 
 export default function AddTags () {
+    const [display, resetDisplay] = useState('display-off')
+    
+    function handleChange () {
+        if (display == 'display-on') {
+        resetDisplay('display-off')
+        }
+        else resetDisplay('display-on')
+    }
+    
+
+
     return (
         <div class="input-area"> 
             <div class="input-sections">
                 <p class="input-name">Select industry</p>
-                <input placeholder="select industry" />
+                <input placeholder="select industry" onClick={handleChange}/>
+                <div className={display} >hello</div>
+             
             </div>
+            
 
             <div  class="input-sections">
                 <p class="input-name">Additional Filters</p>
--- compare-to-peers/src/pages/HeroSection/RaiseGraph.js
+++ compare-to-peers/src/pages/HeroSection/RaiseGraph.js
@@ -1,9 +1,13 @@
 import {Context} from '../utils/providers/context'
 import React, { useContext, useState } from 'react'
+import styled from 'styled-components';
+import { formatPrice } from '../utils/providers/index';
 
 export default function RaiseGraph () {
     const [firstValue, setFirstValue] = useState(750000)
     const [secondValue, setSecondValue] = useState(3000000)
+    let firstValueUsd = formatPrice(firstValue)
+    let secondValueUsd = formatPrice(secondValue)
     
     const {handleRaiseValues, raiseValues} = useContext(Context)
     
@@ -22,6 +26,22 @@ export default function RaiseGraph () {
         handleRaiseValues(raiseValues[0], max)
     }
 
+    
+    const Output = styled.output`
+    display: inline-block;
+    position: absolute;
+    color: white;
+    padding: .5em;
+    top: 20px;
+  `
+  function findLeftPosition(actualValue, min, max){
+    let value = parseInt(actualValue, 10),
+    minValue = min, 
+    maxValue = max
+    const newVal = Number(((value - minValue) * 100) / (maxValue - minValue));
+    const left = `calc(${newVal}% + (${- 35 - newVal * 0.15}px))`
+    return left
+  }
 
 
     return (
@@ -30,9 +50,20 @@ export default function RaiseGraph () {
         <h2>Raise Goal</h2>
         <div class="input-range-area">
         <input class="range" type="range" min={50000} max={2000002} value={firstValue} onChange={handleOnChange1} tabindex="1"  />
-        <Output />
+
+        <Output style={{left: findLeftPosition(firstValue, 50000, 2000001)}}>
+        {firstValueUsd}
+        
+        
+        </Output>
     
        <input class="overlay-value" type="range" min={2000002} max={5000000} value={secondValue} onChange={handleOnChange2} tabindex="0"  />
+       <Output id="output" style={{left: findLeftPosition(secondValue, 2000002, 5000000)}}>
+       {secondValueUsd}
+
+    
+
+       </Output>
      
        </div>
 
