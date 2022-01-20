7b2d060eec6d361bc973455e93fe441e6198b882
Relenting <paulsanchezg24@gmail.com>
2022-01-20 11:49:55 -0500

    Added onClick to div tag

--- compare-to-peers/src/pages/HeroSection/Tag.js
+++ compare-to-peers/src/pages/HeroSection/Tag.js
@@ -20,9 +20,9 @@ export default function Tag({
     }
 
     return (
-    <div class="dropdown-menu">
+    <div class="dropdown-menu" onClick={handleClick}>
         {tag.label}
-        <img onClick={handleClick} src={closeBtn}  />
+        <img src={closeBtn} />
     </div>
     )
 }
