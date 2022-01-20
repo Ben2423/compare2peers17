dd4fd23c78721136eab7f69fb5f4526d06bfd8b7
Ben2423 <benlappen@gmail.com>
2022-01-19 18:51:35 -0800

    added log for displaying tags to filter companies

--- compare-to-peers/src/pages/HeroSection/AddTags.js
+++ compare-to-peers/src/pages/HeroSection/AddTags.js
@@ -1,8 +1,18 @@
+//dependencies 
 import styled from 'styled-components';
 import React, {useState} from 'react'
 
+//JSON data
+import originalIndustryTags from '../../data/industryTags.json/industryTags.json'
+
+// imported pages
+import Tag from './Tag'
 
 export default function AddTags () {
+//original company tags data to be displayed via drop down
+    const [originalDataTags, setOriginalDataTags] = useState(originalIndustryTags)
+
+
     const [display, resetDisplay] = useState('display-off')
     
     function handleChange () {
@@ -19,7 +29,13 @@ export default function AddTags () {
             <div class="input-sections">
                 <p class="input-name">Select industry</p>
                 <input placeholder="select industry" onClick={handleChange}/>
-                <div className={display} >hello</div>
+                <div className={display}>
+                    {originalDataTags.map(tag=> {
+                        return (
+                            <Tag tag={tag} />
+                        )
+                    })}
+                </div>
              
             </div>
             
---
+++ compare-to-peers/src/pages/HeroSection/Tag.js
@@ -0,0 +1,11 @@
+
+
+
+export default function Tag({tag}) {
+    return (
+    <div class="dropdown-menu">
+        {tag.label}
+
+    </div>
+    )
+}
