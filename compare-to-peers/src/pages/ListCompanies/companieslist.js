98b39ced07a59efd58d32fbd1c45707134e38174
Ben2423 <benlappen@gmail.com>
2022-01-23 21:16:15 -0800

    attached 2 pointer slider graph to displayed company card list

--- compare-to-peers/src/pages/ListCompanies/companieslist.js
+++ compare-to-peers/src/pages/ListCompanies/companieslist.js
@@ -1,23 +1,37 @@
 import React, {useContext, useState} from 'react'
 import CompanyCard from './CompanyCard'
 import { ContextTags } from '../utils/providers/tagsdata'
+import { Context } from '../utils/providers/context'
+
+
 
 export default function ListCompanies ({
 
 }) {
 
+    const {raiseValues} = useContext(Context)
     const {companyJsonData} = useContext(ContextTags)
+
+function renderCompanies(){
+        let items = []
+        if(companyJsonData.length > 0){
+            for(const company of companyJsonData){
+                if(company.raise >= raiseValues[0] && company.raise <= raiseValues[1]){
+                    items.push(
+                        <CompanyCard company={company}/>
+                    )
+                }
+            }
+        }
+        if(items.length === 0) return <p>No company found within that range! Try changing the range</p>
+        return items
+}
 
     return (
         <div>
         
             <div id="company-display-area">
-            {companyJsonData.map(company=> {
-                return (
-                    <CompanyCard company={company}/>
-                )
-            })}
-                
+                    {renderCompanies()}
             </div>
         
         </div>
